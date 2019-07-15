47647d54169a4b0cb8bfbcefbe6e274aa91a1605
Павел Смирнов <Pasha@MacBook-Air-Pavel.local>
2019-07-16 01:01:03 +0300

    Initial commit: Add Learn Next.js; Commiter: Pavel Smirnov, 1:00

---
+++ 2-using-shared-components/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+//import Header from '../components/Header';
+import Layout from '../components/MyLayout.js';
+
+
+export default function Index() {
+  return (
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  )
+}
